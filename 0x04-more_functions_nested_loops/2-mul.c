264223f2ef306710e05efbff9480299f69d1961a
Lunga <xolanidadajada@gmail.com>
2023-06-22 13:00:57 -0700

    No description found for return value of 'mul'

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,14 +3,14 @@
 
 /**
  * mul - Multiplies two integers
- * @x: The first number to multiply
- * @y: The second number to multiplication  between x  param and y param
+ * @a: The first number to multiply
+ * @b: The second number to multiplication  between a  param and b param
  */
 
-int mul(int x, int y)
+int mul(int a, int b);
 
 {
 
-	return (x * y);
+	return (a * b);
 
 }
