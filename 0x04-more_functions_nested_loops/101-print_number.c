a7afe42ce3a99865eb8e45d13bdd403b4d6668cf
Lunga <xolanidadajada@gmail.com>
2023-06-22 15:32:49 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,12 +14,12 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-	n *= -1;
-	k = n;
-	_putchar('_');
+		n *= -1;
+		k = n;
+		_putchar('-');
 	}
 	k /= 10;
 	if (k != 0)
-	print_number(k);
+		print_number(k);
 	_putchar((unsigned int) n % 10 + '0');
 }
